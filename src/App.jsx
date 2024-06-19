63debfab736786502527928bbf767e8fac85ff6e
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-06-19 09:20:34 -0500

    finished header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
 import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Cart from './pages/cart/Cart'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
 
 export default function App() {
     return (
         <div className='app'>
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/order" element={<PlaceOrder />} />
+            </Routes>
         </div>
     )
 }
