e6f0be09b02165eb2b6488c924d0840ff07b582d
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-06-19 11:45:27 -0500

    finished foodDisplay component and functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../components/header/Header'
+import ExploreMenu from '../../components/exploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 
 export default function Home() {
+    const [category, setCategory] = useState("all")
+
     return (
         <div>
             <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
+            <FoodDisplay category={category} />
         </div>
     )
 }
